fe602d743a53666b60834d33ac5945dc26ec8a05
Gabriel Zayas <g-zayas@hotmail.com>
2022-06-24 20:16:58 +0900

    Throw error properly when user isn't authorized to perform API action

--- app/controllers/api/v1/loads_and_authorizes_api_resource.rb
+++ app/controllers/api/v1/loads_and_authorizes_api_resource.rb
@@ -20,22 +20,31 @@ module Api::V1::LoadsAndAuthorizesApiResource
           /_id$/i.match?(param)
         }
 
-        all_accessible_api_resources = api_resource_class.accessible_by(current_ability, permission)
+        unless permission.eql? :create
+          resource_type = api_resource_param_id.present? ? :single_record : :collection
+          resources = api_resource_class.accessible_by(current_ability, permission)
 
-        if api_resource_param_id.present? # :read, :update, :delete
+          # If there no other ids (i.e. if we're doing a `show` action),
+          # we simply get the original resources array.
+          all_accessible_api_resources = resources.where(api_resource_params_other_ids)
+          raise CanCan::AccessDenied if all_accessible_api_resources.empty?
+        end
+
+        if resource_type == :single_record # :show, :update, :destroy
           instance_variable_set(instance_variable_name, all_accessible_api_resources.find(api_resource_param_id))
-        elsif permission.eql? :create
-          instance_variable_set(instance_variable_name, api_resource_class.new(api_resource_params))
-        elsif permission.eql? :read
-          all_accessible_api_resources = all_accessible_api_resources.where(api_resource_params_other_ids) if api_resource_params_other_ids.present?
+        elsif resource_type == :collection # index
           instance_variable_set(instance_variable_collection_name, all_accessible_api_resources)
           skip_authorize = true # can't use CanCan to authorize collections
+        elsif permission.eql? :create
+          instance_variable_set(instance_variable_name, api_resource_class.new(api_resource_params))
         end
 
         eval "authorize! :#{permission}, #{instance_variable_name}" unless skip_authorize
       rescue ActiveRecord::RecordNotFound
         # the default RecordNotFound message includes the raw SQL... which feels bad
         handle_api_error(ActiveRecord::RecordNotFound.new("The id #{api_resource_param_id} could not be found."))
+      rescue CanCan::AccessDenied
+        handle_api_error(CanCan::AccessDenied.new("You are not authorized to access this data.", permission, api_resource_class))
       end
     end
   end
