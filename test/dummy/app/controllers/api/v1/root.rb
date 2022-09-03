660ddc54d81adfbbebbd86e8702e8f193aa41c84
Gabriel Zayas <g-zayas@hotmail.com>
2022-09-03 11:24:51 +0900

    Comment out out-dated dummy app code

--- test/dummy/app/controllers/api/v1/root.rb
+++ test/dummy/app/controllers/api/v1/root.rb
@@ -1,7 +1,9 @@
-class Api::V1::Root < Api::Base
-  include Api::V1::Base
-
-  # 🚅 super scaffolding will mount new endpoints above this line.
-
-  handle_not_found
-end
+# TODO: These will either be updated or removed soon
+# since we will switch to using vanilla Rails API controllers.
+# class Api::V1::Root < Api::Base
+#   include Api::V1::Base
+#
+#   # 🚅 super scaffolding will mount new endpoints above this line.
+#
+#   handle_not_found
+# end
