03c545db9df6fd5273f2b678e5014bb77698254a
Gabriel Zayas <g-zayas@hotmail.com>
2022-09-03 11:40:07 +0900

    Use native Rails API controllers in dummy app

---
+++ test/dummy/app/controllers/api/v1/root.rb
@@ -0,0 +1,5 @@
+class Api::V1::Root < ActionController::API
+  # 🚅 super scaffolding will mount new endpoints above this line.
+
+  handle_not_found
+end
---
+++ test/dummy/app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::TeamsController < ActionController::API
+  PARAMS = proc do
+    # 🚅 super scaffolding will insert new fields above this line.
+    # 🚅 super scaffolding will insert new arrays above this line.
+    # 🚅 super scaffolding will insert processing for new fields above this line.
+  end
+end
